44d3833fc3044e067063bcfc9feff789d02d3e26
onbarkas <onbarkas@inbox.lv>
2019-03-04 18:10:24 +0200

    homework3

---
+++ src/lv/IfHomework/LightColorDetectorTest.java
@@ -0,0 +1,8 @@
+package lv.IfHomework;
+
+public class LightColorDetectorTest {
+    public static void main(String[] args) {
+        LightColorDetector ColorDetect = new LightColorDetector();
+        System.out.println(ColorDetect.detect(700));
+    }
+}
