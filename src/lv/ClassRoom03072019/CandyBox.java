9f01406c36971e2ee26428e70133a1313bd25937
onbarkas <onbarkas@inbox.lv>
2019-03-07 20:57:45 +0200

    ClassWork

--- src/lv/ClassRoom03072019/CandyBox.java
+++ src/lv/ClassRoom03072019/CandyBox.java
@@ -1,5 +1,7 @@
 package lv.ClassRoom03072019;
 
+import java.util.Objects;
+
 public class CandyBox {
     private String name;
     private int amount;
@@ -16,6 +18,27 @@ public class CandyBox {
 
     public int getAmount() {
         return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        CandyBox candyBox = (CandyBox) o;
+        return this.amount == candyBox.amount
+                && this.name.equals(candyBox.name);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(name, amount);
+    }
+
+    @Override
+    public String toString() {
+        return "CandyBox: {name: " + name
+                + ", amount: " + amount
+                + "}";
     }
 
 }
--- src/lv/ClassRoom03072019/CandyBoxTest.java
+++ src/lv/ClassRoom03072019/CandyBoxTest.java
@@ -2,7 +2,21 @@ package lv.ClassRoom03072019;
 
 public class CandyBoxTest {
     public static void main(String[] args) {
+        TestCandyBoxToString();
+    }
+
+    private static void TestCandyBoxToString() {
         CandyBox candyBox = new CandyBox("Prozit", 8);
+
+        String expected = "CandyBox: {name: Prozit, amount: 8}";
+
+        System.out.print("ToString Test ");
+
+        if (expected.equals(candyBox.toString())) {
+            System.out.println("PASSED");
+        } else{
+            System.out.println("FAILED");
+        }
 
         System.out.println("CandyBox name: " + candyBox.getName() + ", amount: " + candyBox.getAmount());
 
