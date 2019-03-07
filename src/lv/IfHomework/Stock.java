0b323649380805d7a7c1445c971c5089915a9777
onbarkas <onbarkas@inbox.lv>
2019-03-07 20:12:50 +0200

    ClassWork

--- src/lv/IfHomework/Stock.java
+++ src/lv/IfHomework/Stock.java
@@ -1,38 +1,49 @@
 package lv.IfHomework;
 
-public class Stock   {
-    String company;
-    double currentValue;
-    double min;
-    double max;
+public class Stock {
+    private String company;
+    private double currentValue;
+    private double min;
+    private double max;
 
+    public Stock(String company, double currentValue) {
+        this.company = company;
+        setCurrentValue(currentValue);
+    }
 
-    public String getCompany(){
+    public String getCompany() {
         return company;
     }
 
-    public void setCompany (String company){this.company = company;}
+    public void setCompany(String company) {
+        this.company = company;
+    }
 
-    public double getCurrentValue(){
+    public double getCurrentValue() {
         return currentValue;
     }
 
-    public void setCurrentValue(double currentValue){
+    public void setCurrentValue(double currentValue) {
         this.currentValue = currentValue;
         min = currentValue;
-        max = currentValue;}
+        max = currentValue;
+    }
 
-    public double getMax(){
+    public double getMax() {
         return max;
     }
 
-    public void setMax(double max){this.max = max;}
+    public void setMax(double max) {
+        this.max = max;
+    }
 
-    public double getMin(){
+    public double getMin() {
         return min;
     }
 
-    public void setMin(double min){this.min = min;}
+    public void setMin(double min) {
+        this.min = min;
+    }
 
     public void updatePrice(double updateCurrentValue) {
         if (updateCurrentValue > max) {
@@ -44,9 +55,10 @@ public class Stock   {
         currentValue = updateCurrentValue;
     }
 
-    public void printInformation(){
-            System.out.println("Current value = " + currentValue);
-            System.out.println("Max = " + max);
-            System.out.println("Min = " + min);
+
+    public void printInformation() {
+        System.out.println("Current value = " + currentValue);
+        System.out.println("Max = " + max);
+        System.out.println("Min = " + min);
     }
 }
--- src/lv/IfHomework/StockTest.java
+++ src/lv/IfHomework/StockTest.java
@@ -4,9 +4,9 @@ import java.sql.SQLOutput;
 
 public class StockTest {
     public static void main(String[] args) {
-        Stock IBM = new Stock();
-        IBM.setCompany("IBM");
-        IBM.setCurrentValue(150.00);
+        Stock IBM = new Stock("IBM", 150);
+//        IBM.setCompany("IBM");
+//        IBM.setCurrentValue(150.00);
         IBM.updatePrice(150.37);
         IBM.updatePrice(149.82);
         IBM.updatePrice(150.12);
