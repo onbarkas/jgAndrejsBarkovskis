3401093c000213a1dc52d8f85dc94d33e13869fe
onbarkas <onbarkas@inbox.lv>
2019-03-12 16:42:43 +0200

    homework objekti atminja

---
+++ src/lv/ObjektiAtminja/Counter.java
@@ -0,0 +1,47 @@
+package lv.ObjektiAtminja;
+
+public class Counter {
+    private int value = 0;
+    private int step = 1;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        if ((value > 0) && (value < 10)) {
+            this.value = value;
+        } else {
+            clear();
+        }
+
+    }
+
+    public int getStep() {
+        return step;
+    }
+
+    public void setStep(int step) {
+        if ((step > 0) && (step < 100)) {
+            this.step = step;
+        } else {
+            clear();
+        }
+    }
+
+    public void increment() {
+        if ((getValue() + getStep()) <= 100) {
+            value = value + step;
+        }
+    }
+
+    public void decrement() {
+        if ((getValue() - getStep()) < 0) {
+            value = value - step;
+        }
+    }
+
+    public void clear() {
+        value = 0;
+    }
+}
---
+++ src/lv/ObjektiAtminja/CounterTest.java
@@ -0,0 +1,30 @@
+package lv.ObjektiAtminja;
+
+public class CounterTest {
+    public static void main(String[] args) {
+        Counter counterTest = new Counter();
+
+        counterTest.getValue();
+        counterTest.getStep();
+
+        counterTest.increment();
+        counterTest.getValue();
+
+        counterTest.setStep(3);
+        counterTest.increment();
+        counterTest.getValue();
+
+        counterTest.setStep(2);
+        counterTest.decrement();
+        counterTest.getValue();
+
+        counterTest.clear();
+        counterTest.getValue();
+        counterTest.setValue(50);
+
+        counterTest.getValue();
+
+
+
+    }
+}
