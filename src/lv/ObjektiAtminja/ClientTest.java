b6c77b07b7fd8da9e8b2d1a6231d2af41f533efe
onbarkas <onbarkas@inbox.lv>
2019-03-13 13:42:13 +0200

    homework objekti atminja

---
+++ src/lv/ObjektiAtminja/ClientTest.java
@@ -0,0 +1,19 @@
+package lv.ObjektiAtminja;
+
+public class ClientTest {
+    public static void main(String[] args) {
+        ContactInformation contactInformation = new ContactInformation();
+        contactInformation.setAddress("Lacplesa 37");
+        contactInformation.setCity("Riga");
+        contactInformation.setCountry("Latvija");
+        contactInformation.setEmail("Jhon@gmail.com");
+        contactInformation.setPhoneNumber("+37128773678");
+
+        Client client = new Client();
+        client.setFirstName("Jhon");
+        client.setLastName("Doe");
+        client.setPersonalCode("010101-12345");
+        client.setContactInformation(contactInformation);
+        System.out.println(client);
+    }
+}
