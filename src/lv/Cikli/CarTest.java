b7f5681a9df584e893e4a623fda401d8e9369f52
onbarkas <onbarkas@inbox.lv>
2019-03-18 15:14:39 +0200

    homework objekti atminja

--- src/lv/Cikli/CarTest.java
+++ src/lv/Cikli/CarTest.java
@@ -8,6 +8,19 @@ public class CarTest {
         car.setMaxSpeed(200);
         car.setCurrentSpeed(100);
         car.speedUp();
-        System.out.println(car.getCurrentSpeed());
+        System.out.println(car.getModel() + " " + car.getColor() + " " + car.getCurrentSpeed());
+        if (car.isDriving()) {
+            System.out.println("Mashina brauc");
+        } else{
+            System.out.println("Masina nebrauc");
+        }
+        car.slowDown();
+        System.out.println("Jauns atrums ir " + car.getCurrentSpeed());
+
+        if (car.canAccelerate()) {
+            System.out.println("Mashina var braukt par " + (car.getMaxSpeed()-car.getCurrentSpeed() + " atrak"));
+        } else{
+            System.out.println("Masina nevar braukt atrak");
+        }
     }
 }
