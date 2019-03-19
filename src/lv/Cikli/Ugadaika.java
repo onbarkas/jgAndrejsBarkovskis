392e1fe9723d969d86ebd8ed750a49780e7674b3
onbarkas <onbarkas@inbox.lv>
2019-03-19 21:16:52 +0200

    ClassWork

--- src/lv/Cikli/Ugadaika.java
+++ src/lv/Cikli/Ugadaika.java
@@ -4,23 +4,55 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Ugadaika {
-    public int inputInt;
-    public String inputString;
-    public int randomNumber;
-    public int rangeMax = 101;
+    private int inputInt;
+    private String inputString;
+    private int randomNumber;
+    private int rangeMax = 101;
     Scanner scanner = new Scanner(System.in);
     Random randomGenerator = new Random();
+
+    public int getInputInt() {
+        return inputInt;
+    }
+
+    public void setInputInt(int inputInt) {
+        this.inputInt = inputInt;
+    }
+
+    public String getInputString() {
+        return inputString;
+    }
+
+    public void setInputString(String inputString) {
+        this.inputString = inputString;
+    }
+
+    public int getRandomNumber() {
+        return randomNumber;
+    }
+
+    public void setRandomNumber(int randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+
+    public int getRangeMax() {
+        return rangeMax;
+    }
+
+    public void setRangeMax(int rangeMax) {
+        this.rangeMax = rangeMax;
+    }
 
     public void numberCheck() {
         System.out.println("Ievadiet numuru no 1 lidz 100");
         scannerInt();
-        randomNumber = randomGenerator.nextInt(rangeMax);
+        randomNumber = randomGenerator.nextInt((rangeMax - 0) + 1) + 0;
         System.out.println(randomNumber);
         checkLoop();
     }
 
     public void checkLoop() {
-        do{
+        do {
             System.out.println("ievadiet, skaitlis ir lielaks, vienads vai mazaks");
             scannerString();
             if (inputString.equals("lielaks")) {
