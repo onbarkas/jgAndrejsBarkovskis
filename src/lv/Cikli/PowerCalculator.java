6f34e9bfaf793e8fe65c9ebe35005607bad40ee3
onbarkas <onbarkas@inbox.lv>
2019-03-19 10:45:36 +0200

    homework objekti atminja

--- src/lv/Cikli/PowerCalculator.java
+++ src/lv/Cikli/PowerCalculator.java
@@ -1,7 +1,15 @@
 package lv.Cikli;
 
 public class PowerCalculator {
-    public int sum = 1;
+    private int sum = 1;
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
 
     public int pow(int number, int power) {
         for (int i = 1; i <= power; i++) {
