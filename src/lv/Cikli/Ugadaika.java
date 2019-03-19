7dd108f471ec40c4cea86195ff9056826ae77c5a
onbarkas <onbarkas@inbox.lv>
2019-03-19 11:33:55 +0200

    homework objekti atminja

---
+++ src/lv/Cikli/Ugadaika.java
@@ -0,0 +1,31 @@
+package lv.Cikli;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ugadaika {
+    public int input;
+    public int randomNumber;
+    Scanner scanner = new Scanner(System.in);
+    Random randomGenerator = new Random();
+
+    public void numberCheck(){
+        System.out.println("Ievadiet numuru no 1 lidz 100");
+        scanner();
+        if ((input > 0) && (input <= 100)){
+
+        } else {System.out.println("Ievadiet numuru no 1 lidz 100");}
+    }
+
+    public void scanner(){
+        input = scanner.nextInt();
+    }
+
+    public void random(){
+        randomNumber = randomGenerator.nextInt(101);
+    }
+
+
+
+
+}
---
+++ src/lv/Cikli/UgadaikaTest.java
@@ -0,0 +1,12 @@
+package lv.Cikli;
+
+
+public class UgadaikaTest {
+    public static void main(String[] args) {
+        Ugadaika ugadaika = new Ugadaika();
+
+        ugadaika.numberCheck();
+        ugadaika.random();
+        System.out.println(ugadaika.randomNumber);
+    }
+}
