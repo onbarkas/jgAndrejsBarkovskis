eee236a8c5f3e6e20ceaca3a01e076137810aafe
onbarkas <onbarkas@inbox.lv>
2019-03-24 16:53:05 +0200

    homework/lesson7

--- src/lv/homework/lesson7/PalindromCheck.java
+++ src/lv/homework/lesson7/PalindromCheck.java
@@ -11,7 +11,7 @@ public class PalindromCheck {
 
         String[] array = new String[5];
         String[] arrayReversed = new String[5];
-        array[0] = "racecar";
+        array[0] = "Racecar";
         array[1] = "level";
         array[2] = "level";
         array[3] = "summus";
@@ -20,13 +20,12 @@ public class PalindromCheck {
         System.out.println(Arrays.toString(array));
 
         for (int i = 0; i < array.length; i++) {
-            arrayReversed[i] = new StringBuilder(array[i]).reverse().toString();
+            arrayReversed[i] = new StringBuilder(array[i]).reverse().toString().toLowerCase();
+            array[i] = new StringBuilder(arrayReversed[i]).reverse().toString().toLowerCase();
         }
 
-
-
         do {
-            System.out.println("Ievadiet rindas numuru no 0 lidz " + array.length + ", kuru gribat parbaudit");
+            System.out.println("Ievadiet rindas numuru no 0 lidz " + (array.length-1) + ", kuru gribat parbaudit");
             inputInt = scanner.nextInt();
 
         } while ((inputInt < 0) || (inputInt > 4));
@@ -38,7 +37,6 @@ public class PalindromCheck {
         } else {
             System.out.println("nav");
         }
-
 
 
     }
