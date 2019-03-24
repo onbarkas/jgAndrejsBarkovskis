6e58c51087d458c54ffa2c378f1295eed4d04e17
onbarkas <onbarkas@inbox.lv>
2019-03-24 09:28:40 +0200

    homework/lesson2

---
+++ src/lv/homework/lesson2/MathOperations.java
@@ -0,0 +1,17 @@
+package lv.homework.lesson2;
+
+public class MathOperations {
+    public static void main(String[] args) {
+        int firstNumber = 10, secondNumber = 15;
+
+        int sumResult = firstNumber + secondNumber;
+        int subtract = firstNumber - secondNumber;
+        int divide = firstNumber / secondNumber;
+        int multiply = firstNumber * secondNumber;
+
+        System.out.println(sumResult);
+        System.out.println(subtract);
+        System.out.println(divide);
+        System.out.println(multiply);
+    }
+}
