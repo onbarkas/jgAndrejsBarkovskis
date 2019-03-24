cd34dd088517d384b3421bd4d66cb8071699012a
onbarkas <onbarkas@inbox.lv>
2019-03-24 17:38:22 +0200

    homework/lesson7

--- src/lv/homework/lesson7/PalindromCheck.java
+++ src/lv/homework/lesson7/PalindromCheck.java
@@ -1,48 +1,50 @@
 package lv.homework.lesson7;
 
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class PalindromCheck {
-    public void isPalindrome() {
+    public boolean isPalindrome() {
         int inputInt;
         Scanner scanner = new Scanner(System.in);
-
         String[] array = new String[5];
         String[] arrayReversed = new String[5];
+
         array[0] = "Racecar";
         array[1] = "level";
-        array[2] = "level";
+        array[2] = "Level";
         array[3] = "summus";
         array[4] = "auto";
 
-        System.out.println(Arrays.toString(array));
+        listOfArray(array);
 
+        reverse(array, arrayReversed);
+
+        inputInt = inputChecker(scanner, array);
+
+        return (array[inputInt].equals(arrayReversed[inputInt]));
+
+    }
+
+    private void listOfArray(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println((i + 1) + ". " + array[i]);
+        }
+    }
+
+    private void reverse(String[] array, String[] arrayReversed) {
         for (int i = 0; i < array.length; i++) {
             arrayReversed[i] = new StringBuilder(array[i]).reverse().toString().toLowerCase();
             array[i] = new StringBuilder(arrayReversed[i]).reverse().toString().toLowerCase();
         }
-
-        inputInt = inputChecker(scanner, array);
-
-        System.out.println(array[inputInt]);
-        System.out.println(arrayReversed[inputInt]);
-        if (array[inputInt].equals(arrayReversed[inputInt])) {
-            System.out.println("ir");
-        } else {
-            System.out.println("nav");
-        }
-
-
     }
 
     private int inputChecker(Scanner scanner, String[] array) {
         int inputInt;
         do {
-            System.out.println("Ievadiet rindas numuru no 0 lidz " + (array.length-1) + ", kuru gribat parbaudit");
-            inputInt = scanner.nextInt();
-        } while ((inputInt < 0) || (inputInt > 4));
+            System.out.println("Ievadiet rindas numuru no 1 lidz " + (array.length) + ", kuru gribat parbaudit");
+            inputInt = scanner.nextInt() - 1;
+        } while ((inputInt < 0) || (inputInt > 5));
         return inputInt;
     }
 }
--- src/lv/homework/lesson7/PalindromCheckTest.java
+++ src/lv/homework/lesson7/PalindromCheckTest.java
@@ -3,6 +3,11 @@ package lv.homework.lesson7;
 public class PalindromCheckTest {
     public static void main(String[] args) {
         PalindromCheck palindromCheck = new PalindromCheck();
-        palindromCheck.isPalindrome();
+
+        if (palindromCheck.isPalindrome()) {
+            System.out.println("Rinda ir palindroms");
+        } else{
+            System.out.println("Rinda nav palindroms");
+        }
     }
 }
