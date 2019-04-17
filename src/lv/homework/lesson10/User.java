02f8cc69c70e0f9b187ccfde85ad7dc536d27963
onbarkas <onbarkas@inbox.lv>
2019-04-17 20:53:58 +0300

    lesson/lesson10

---
+++ src/lv/homework/lesson10/User.java
@@ -0,0 +1,43 @@
+package lv.homework.lesson10;
+
+public class User {
+    private String name, userName;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean userValidationService() {
+        if (age <= 0 || age >= 120) {
+            return false;
+        }
+        if (name.length() < 3 || name.length() > 15 || name.equals("")) {
+            return false;
+        }
+        if (userName.length() < 3 || userName.length() > 15 || userName.equals("")) {
+            return false;
+        }
+        return true;
+    }
+}
