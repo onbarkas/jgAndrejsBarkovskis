2faaa11848a3587ad0d4107557704473f41c9683
onbarkas <onbarkas@inbox.lv>
2019-04-17 21:12:10 +0300

    lesson/lesson10

--- src/lv/homework/lesson10/User.java
+++ src/lv/homework/lesson10/User.java
@@ -4,6 +4,21 @@ public class User {
     private String name, userName;
     private int age;
 
+    public User(String name, String userName, int age) throws IllegalAccessException {
+        if (age <= 0 || age >= 120){
+            throw new IllegalAccessException("age only can be from 0 to 120");
+        }
+        if (name.length() < 3 || name.length() > 15 ||  name.isEmpty()){
+            throw new IllegalAccessException("name can consist from 3 to 15 symbols");
+        }
+        if (userName.length() < 3 || userName.length() > 15 || userName.equals("")){
+            throw new IllegalAccessException("user name can consist from 3 to 15 symbols");
+        }
+        this.name = name;
+        this.userName = userName;
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
