45fced77477fbdf46e95ca520b2296c0ba605f9f
onbarkas <onbarkas@inbox.lv>
2019-04-17 21:34:36 +0300

    lesson/lesson10

--- src/lv/homework/lesson10/UserTest.java
+++ src/lv/homework/lesson10/UserTest.java
@@ -3,5 +3,6 @@ package lv.homework.lesson10;
 public class UserTest {
     public static void main(String[] args) {
         User user = new User("Andrejs", "Barkovskis", 32);
+        System.out.println(user.userValidationService() ? "validation success" : "validation unseccessfull");
     }
 }
