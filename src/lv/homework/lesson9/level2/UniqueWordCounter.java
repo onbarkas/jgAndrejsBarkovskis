9b5c76306b45c1c8b301c29de48eef2db17ccfbe
onbarkas <onbarkas@inbox.lv>
2019-04-21 23:22:34 +0300

    homework/lesson9/finished

--- src/lv/homework/lesson9/level2/UniqueWordCounter.java
+++ src/lv/homework/lesson9/level2/UniqueWordCounter.java
@@ -1,4 +1,25 @@
 package lv.homework.lesson9.level2;
 
-public class UniqueWordCounter {
+import java.util.HashMap;
+import java.util.Map;
+
+class UniqueWordCounter {
+    private Map<String, Integer> vocabulary = new HashMap<>();
+    private Integer keyNumber = 1;
+    private Integer counter = 1;
+
+    void addWord(String word) {
+        if (word.isEmpty()) {
+        } else if (vocabulary.containsKey(word)) {
+            vocabulary.replace(word, counter = counter + 1);
+        } else {
+            vocabulary.put(word, keyNumber);
+        }
+    }
+
+    void printToConsole() {
+        System.out.println(vocabulary);
+    }
 }
+
+
---
+++ src/lv/homework/lesson9/level2/UniqueWordCounterTest.java
@@ -0,0 +1,14 @@
+package lv.homework.lesson9.level2;
+
+public class UniqueWordCounterTest {
+    public static void main(String[] args) {
+        UniqueWordCounter uniqueWordCounter = new UniqueWordCounter();
+        uniqueWordCounter.addWord("Pirmais");
+        uniqueWordCounter.addWord("Pirmais");
+        uniqueWordCounter.addWord("otrais");
+        uniqueWordCounter.addWord("treshais");
+        uniqueWordCounter.addWord("ceturtais");
+        uniqueWordCounter.addWord("Pirmais");
+        uniqueWordCounter.printToConsole();
+    }
+}
