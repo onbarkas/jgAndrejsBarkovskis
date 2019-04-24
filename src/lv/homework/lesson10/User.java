12b115afe0199474bca7ce7af6305eb98d32e09f
onbarkas <onbarkas@inbox.lv>
2019-04-24 22:32:31 +0300

    homework/lesson9/level3/unfinished

--- src/lv/homework/lesson10/User.java
+++ src/lv/homework/lesson10/User.java
@@ -3,21 +3,21 @@ package lv.homework.lesson10;
 import javax.xml.bind.ValidationException;
 
 public class User {
-    private String name, userName;
+    private String name, lastName;
     private int age;
 
-    public User(String name, String userName, int age) throws ValidationException {
+    public User(String name, String lastName, int age) throws ValidationException {
         if (age <= 0 || age >= 120) {
             throw new ValidationException("age only can be from 0 to 120");
         }
-        if (name.length() < 3 || name.length() > 15 || name.isEmpty()) {
-            throw new ValidationException("name can consist from 3 to 15 symbols");
+        if (name.length() < 3 || name.length() > 15) {
+            throw new ValidationException("Name can consist from 3 to 15 symbols");
         }
-        if (userName.length() < 3 || userName.length() > 15 || userName.equals("")) {
-            throw new ValidationException("user name can consist from 3 to 15 symbols");
+        if (lastName.length() < 3 || lastName.length() > 15) {
+            throw new ValidationException("Last name can consist from 3 to 15 symbols");
         }
         this.name = name;
-        this.userName = userName;
+        this.lastName = lastName;
         this.age = age;
     }
 
@@ -25,10 +25,10 @@ public class User {
         if (age <= 0 || age >= 120) {
             return false;
         }
-        if (name.length() < 3 || name.length() > 15 || name.isEmpty()) {
+        if (name.length() < 3 || name.length() > 15) {
             return false;
         }
-        if (userName.length() < 3 || userName.length() > 15 || userName.equals("")) {
+        if (lastName.length() < 3 || lastName.length() > 15) {
             return false;
         }
         return true;
