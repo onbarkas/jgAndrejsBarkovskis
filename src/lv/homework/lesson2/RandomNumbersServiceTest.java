6cca10266ddca071f878726c00a7a855314f078f
onbarkas <onbarkas@inbox.lv>
2019-04-28 11:17:49 +0300

    homework/lesson2/level2/finished

--- src/lv/homework/lesson2/RandomNumbersServiceTest.java
+++ src/lv/homework/lesson2/RandomNumbersServiceTest.java
@@ -4,21 +4,23 @@ import java.util.Random;
 
 public class RandomNumbersServiceTest {
     public static void main(String[] args) {
-        int firstNumber = getRandom();
-        int secondNumber = getRandom();
-        int thirdNumber = getRandom();
-
+        Random randomGenerator = new Random();
+        int firstNumber = randomGenerator.nextInt(50);
+        int secondNumber = randomGenerator.nextInt(50);
+        int thirdNumber = randomGenerator.nextInt(50);
         int randomNumbersSum = firstNumber + secondNumber + thirdNumber;
         System.out.println("Random numbers sum: " + randomNumbersSum);
-
     }
 
+/*
     public static int getRandom() {
         Random randomGenerator = new Random(); // katru reizi tiks veidots jauns Random objekts, to labak iznest klases līmenī vai nodot kā parametru
         int randomNumber = randomGenerator.nextInt(50);
-        randomNumber += 1;
+        System.out.println(randomNumber);
+        //randomNumber += 1;
         return randomNumber;
     }
+*/
 
 
 }
