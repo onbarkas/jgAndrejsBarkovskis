72cb1474a3f5a170fc70d773eb37fccbd4c068a8
onbarkas <onbarkas@inbox.lv>
2019-04-29 18:08:43 +0300

    homework/lesson4/level4/unfinished

--- src/lv/homework/lesson4/level4/CreditCard.java
+++ src/lv/homework/lesson4/level4/CreditCard.java
@@ -1,29 +1,37 @@
 package lv.homework.lesson4.level4;
 
-public class CreditCard {
-    int cardID;
-    double debetBilance = 100;
-    double creditUsed = 0;
-    double creditLimit = 200;
-    String pinCode;
-    double availableAmount;
-    double amount;
+class CreditCard {
+    private int cardID;
+    private double debetBilance = 100;
+    private double creditUsed = 150;
+    private double creditLimit = 200;
+    private String pinCode = "STR";
+    private double availableAmount;
+    private double amount;
 
-/*    public void moneyIn() {
-//verifikacija
-        if topup() == true {
+    void moneyIn(double amount, String pinCode) {
+        if (this.pinCode.equals(pinCode)) {
+            creditUsed = creditUsed - amount;
+            if (creditUsed < 0) {
+                debetBilance =  debetBilance + (0 - creditUsed);
+            }
+            System.out.println("ss" + amount + "\n" + creditUsed + "\n" + debetBilance);
+        } else {
+            System.out.println("neizdevas" + amount + pinCode);
         }
+    }
 
-        public void moneyOut () {
+/*        public void moneyOut () {
 //verifikacija
         }*/
 
-        public boolean topup(double amount, String pinCode){
-            return this.pinCode.equals(pinCode) && (amount > 0);
-        }
+/*    public boolean topup(double amount, String pinCode) {
+        return this.pinCode.equals(pinCode)
+                && (amount > this.amount)
+    }*/
 
 /*    boolean isEnoughMoney() {
         if ((availableAmount - amount) > 0.001);{}
     }*/
-    }
+}
 
--- src/lv/homework/lesson4/level4/CreditCardTest.java
+++ src/lv/homework/lesson4/level4/CreditCardTest.java
@@ -2,6 +2,8 @@ package lv.homework.lesson4.level4;
 
 public class CreditCardTest {
     public static void main(String[] args) {
+        CreditCard creditCard = new CreditCard();
+        creditCard.moneyIn( 200, "STR");
 
     }
 }
