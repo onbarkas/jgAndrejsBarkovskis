98fe9c9e1559fd5f6e3bc0218b8016fad0b2fdd1
onbarkas <onbarkas@inbox.lv>
2019-04-29 17:00:54 +0300

    homework/lesson4/level4/unfinished

---
+++ src/lv/homework/lesson4/level4/CreditCard.java
@@ -0,0 +1,29 @@
+package lv.homework.lesson4.level4;
+
+public class CreditCard {
+    int cardID;
+    double debetBilance = 100;
+    double creditUsed = 0;
+    double creditLimit = 200;
+    String pinCode;
+    double availableAmount;
+    double amount;
+
+/*    public void moneyIn() {
+//verifikacija
+        if topup() == true {
+        }
+
+        public void moneyOut () {
+//verifikacija
+        }*/
+
+        public boolean topup(double amount, String pinCode){
+            (!this.pinCode.equals(pinCode) && (amount > 0))
+        }
+
+/*    boolean isEnoughMoney() {
+        if ((availableAmount - amount) > 0.001);{}
+    }*/
+    }
+
---
+++ src/lv/homework/lesson4/level4/CreditCardTest.java
@@ -0,0 +1,7 @@
+package lv.homework.lesson4.level4;
+
+public class CreditCardTest {
+    public static void main(String[] args) {
+
+    }
+}
