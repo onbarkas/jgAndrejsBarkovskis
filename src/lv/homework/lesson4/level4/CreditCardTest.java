f86f98ba06a3da8dd27a000e77c5e42f26df05f1
onbarkas <onbarkas@inbox.lv>
2019-04-30 12:05:37 +0300

    homework/lesson4/level4/finished

--- src/lv/homework/lesson4/level4/CreditCardTest.java
+++ src/lv/homework/lesson4/level4/CreditCardTest.java
@@ -4,7 +4,7 @@ public class CreditCardTest {
     public static void main(String[] args) {
         CreditCard creditCard = new CreditCard();
         creditCard.showBilance("STR");
-        creditCard.moneyIn( 200, "STR");
+        creditCard.moneyIn(200, "STR");
         creditCard.showBilance("STR");
         creditCard.moneyOut(51.54, "STR");
         creditCard.showBilance("STR");
